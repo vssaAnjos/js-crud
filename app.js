da4da9d6f8e2ddfc1cadc3bf77247fe8ca8aa5b4
vssaAnjos <anjnessa9@gmail.com>
2020-05-04 22:56:49 -0300

    add funcionalidade para salvar o input do texbox atividade e salvar no localStorage

--- app.js
+++ app.js
@@ -2,6 +2,31 @@
 // Variaveis Globais
 const formularioUI = document.querySelector('#formulario'); // seleciona tambem classes
 const listaTarefas = document.getElementById('listaTarefas'); // seleciona só ids
+let arrayAtividades = [];
+
+
 // Funcoes
+const CriarItem = (atividade) => {
+    let item = {
+        atividade : atividade,
+        estado : false
+    }
+    arrayAtividades.push(item);
+    return item;
+} 
+
+const GuardarDB = () => {
+    localStorage.setItem('rotina',JSON.stringify(arrayAtividades));
+}
 
 // EventListener
+// para capturar o botão do tipo submit
+formularioUI.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    // capturar o input no textbox com id=atividade
+    let atividadeUI = document.querySelector('#atividade').value;
+    CriarItem(atividadeUI);
+    GuardarDB();
+    formularioUI.reset(); // reiniciar o formulario
+});
+
