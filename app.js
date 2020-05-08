a30674bac0808ac7ab3218bbcaf8104ef44061c4
vssaAnjos <anjnessa9@gmail.com>
2020-05-07 22:54:00 -0300

    add func de salvar novas atividades e exibir na tela conforme click no button

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // console.log('funcionando');
 // Variaveis Globais
 const formularioUI = document.querySelector('#formulario'); // seleciona tambem classes
-const listaTarefas = document.getElementById('listaTarefas'); // seleciona só ids
+const listaTarefasUI = document.getElementById('listaTarefas'); // seleciona só ids
 let arrayAtividades = [];
 
 
@@ -17,6 +17,32 @@ const CriarItem = (atividade) => {
 
 const GuardarDB = () => {
     localStorage.setItem('rotina',JSON.stringify(arrayAtividades));
+    ExibirDB();
+}
+
+const ExibirDB = () => {
+    listaTarefasUI.innerHTML = '';
+    arrayAtividades = JSON.parse(localStorage.getItem('rotina'));
+    console.log(arrayAtividades);
+    if(arrayAtividades === null) {
+        arrayAtividades=[];
+    } else{
+        arrayAtividades.forEach(element => {
+            console.log(element);
+            listaTarefasUI.innerHTML += `  <div class="alert alert-primary" role="alert">
+                <span class="material-icons float-left mr-3">accessibility</span>
+            <b> ${element.atividade}</b> - ${element.estado}
+                <span class="float-right">
+              <span class="material-icons">
+                  check_circle_outline
+              </span> 
+              <span class="material-icons">
+                  remove_circle_outline
+                  </span>
+            </span>
+          </div>`
+        });
+    }
 }
 
 // EventListener
@@ -29,4 +55,6 @@ formularioUI.addEventListener('submit',(e)=>{
     GuardarDB();
     formularioUI.reset(); // reiniciar o formulario
 });
-
+// Evento do DOM
+// Primeiro a executar quando carrega o HTML
+document.addEventListener('DOMContentLoaded',ExibirDB);
